0a7ef382f6ff42aa582715aa6a5dac98f03fad51
sahan <fs.sahan09@gmail.com>
2025-12-14 06:16:47 +0300

    fix url path

--- e-commerce/examples/urls.py
+++ e-commerce/examples/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('products/', include("myApp.url")),
+    path('', include("myApp.url")),
     path("account/", include("account.url")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
